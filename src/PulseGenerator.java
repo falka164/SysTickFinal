00bdf5f220bd1b9215bd148948207dfed2aa1563
blonski <piotrblonski96@gmail.com>
2019-01-22 15:44:09 +0100

    generator v1. Not compiled

--- src/PulseGenerator.java
+++ src/PulseGenerator.java
@@ -1,10 +1,23 @@
 import java.awt.event.ActionListener;
 
-public class PulseGenerator implements IPulseSource {
-
+public class PulseGenerator implements IPulseSource, Runnable {
+        byte mode;
+        boolean isUp;
+        int delay;
+        int burstValue;
+        SysTick= systick;
+        public PulseGenerator(SysTick systick)
+        {
+            this.systick=systick;
+        }
+    @Override
+    public void run(){
+        trigger();
+    }
+        
     @Override
     public void addActionListener(ActionListener pl) {
-
+        
     }
 
     @Override
@@ -14,36 +27,47 @@ public class PulseGenerator implements IPulseSource {
 
     @Override
     public void trigger() {
-
+        switch(mode)
+        {
+            case 1:{
+                systick.tick();
+                wait(ms);
+                break;}
+            case 2:{
+                for(int i=;i<burstValue;i++)
+                    systick.tick();
+                wait(ms);
+                break;}
+        }
     }
 
     @Override
     public void setMode(byte mode) {
-
+        this.mode=mode;
     }
 
     @Override
     public byte getMode() {
-        return 0;
+        return mode;
     }
 
     @Override
     public void halt() {
-
+        isUp=false;
     }
 
     @Override
     public void setPulseDelay(int ms) {
-
+        delay=ms;
     }
 
     @Override
     public int getPulseDelay() {
-        return 0;
+        return delay;
     }
 
     @Override
     public void setPulseCount(int burst) {
-
+        burstValue=burst;
     }
 }
